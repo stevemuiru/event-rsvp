c7845d4653a26c9c41f390f85f90cf6e4e354289
stephen muiru <stevembugua26@gmail.com>
2025-08-25 15:23:36 +0300

    code additions

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ export function EventRSVPform () {
   const [text, setText] = useState('')
   const [email, setEmail] = useState('')
   const [number, setNumber] = useState('')
+  const [diet, setDiet] = useState('')
   const [checkbox, setCheckbox] = useState(false)
-  const [submit, setSubmit] = useState(false)
+  const [submitted, setSubmit] = useState(false)
 
-  const handleChange = (e) => {
-    e.preventDefault()
+  const handleChange = (event) => {
+    event.preventDefault()
     setSubmit(true)
   }
 
@@ -20,26 +21,46 @@ export function EventRSVPform () {
       
       <label htmlFor="name">Name:</label>
      <input type="text" 
-     onChange = {(e) => setText()} required/>
-     value={text}
+     onChange = {(e) => setText(e.target.value)} 
+     placeholder="Your name"
+     required/>
      
      <label htmlFor="email">Email:</label>
      <input type="email" 
-     onChange = {(e) => setEmail()}
-     value={email}
+     onChange = {(e) => setEmail(e.target.value)}
+     placeholder="Your email"
       required/>
 
      <label htmlFor="number">Number of attendes:</label> 
      <input type="number"
-     onChange = {(e) => setNumber()} 
-     value={number}
+     onChange = {(e) => setNumber(e.target.value)} 
+     placeholder="Number of attendes"
       required/>
+
+      <label htmlFor="diet">Dietary Preferences:</label>
+      <input type="text" 
+      onChange={(e) => setDiet(e.target.value)}
+      placeholder="Dietary Preferences (optional)" />
       
       <p>Bringing additional guests?</p>
      <input type="checkbox"
-     onChange = {(e) => setCheckbox(true)}
+     checked={checkbox}
+     onChange = {(e) => setCheckbox(e.target.checked)}
       required/>
+
+      <button type="submit">Submit RSVP</button>
     </form>
+
+    {submitted && (
+      <div class="confirmation">
+        <h2>RSVP submitted:</h2>
+        <p><strong>Name:</strong> {text}</p>
+        <p><strong>Email:</strong> {email}</p>
+        <p><strong>Attendes:</strong> {number}</p>
+        <p><strong>Dietary Preference:</strong> {diet}</p>
+        <p><strong>Bringing additional guests?</strong> {checkbox ? 'Yes' : 'No'}</p>
+      </div>
+    )}
 
     </>
 
